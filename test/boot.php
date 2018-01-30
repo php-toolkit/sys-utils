1959fe826d5f9a8ee7b5217d2efb92687a1ae666
inhere <in.798@qq.com>
2018-01-30 14:45:18 +0800

    update autoload setting

--- test/boot.php
+++ test/boot.php
@@ -10,14 +10,14 @@ date_default_timezone_set('Asia/Shanghai');
 spl_autoload_register(function ($class) {
     $file = null;
 
-    if (0 === strpos($class,'MyLib\StrUtil\Example\\')) {
-        $path = str_replace('\\', '/', substr($class, strlen('MyLib\StrUtil\Example\\')));
+    if (0 === strpos($class,'MyLib\SysUtil\Example\\')) {
+        $path = str_replace('\\', '/', substr($class, strlen('MyLib\SysUtil\Example\\')));
         $file = dirname(__DIR__) . "/example/{$path}.php";
-    } elseif (0 === strpos($class,'MyLib\StrUtil\Test\\')) {
-        $path = str_replace('\\', '/', substr($class, strlen('MyLib\StrUtil\Test\\')));
+    } elseif (0 === strpos($class,'MyLib\SysUtil\Test\\')) {
+        $path = str_replace('\\', '/', substr($class, strlen('MyLib\SysUtil\Test\\')));
         $file = __DIR__ . "/{$path}.php";
-    } elseif (0 === strpos($class,'MyLib\StrUtil\\')) {
-        $path = str_replace('\\', '/', substr($class, strlen('MyLib\StrUtil\\')));
+    } elseif (0 === strpos($class,'MyLib\SysUtil\\')) {
+        $path = str_replace('\\', '/', substr($class, strlen('MyLib\SysUtil\\')));
         $file = dirname(__DIR__) . "/src/{$path}.php";
     }
 
